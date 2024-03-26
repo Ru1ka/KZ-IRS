7d293990881a41e6044a466c86475d88e9c81f52
Rulka <wotb5ks@gmail.com>
2024-03-26 15:11:34 +0300

    init

---
+++ server/main.py
@@ -0,0 +1,10 @@
+def init():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    init()
---
+++ test.py
@@ -0,0 +1,5 @@
+import cv2
+
+cam1 = cv2.VideoCapture('http://student:nto2024@10.128.73.31/mjpg/video.mjpg')
+cam2 = cv2.VideoCapture('http://student:nto2024@10.128.73.38/mjpg/video.mjpg')
+

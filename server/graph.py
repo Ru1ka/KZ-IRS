52c8f9807948ae1301d7f62ff1f4022a36f7acee
Rulka <wotb5ks@gmail.com>
2024-03-28 21:03:06 +0300

    serealize

--- server/graph.py
+++ server/graph.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Line:
     def __init__(self, id, startPos=None, endPos=None):
         self.id = id
@@ -10,6 +13,28 @@ class Line:
             self.startPos = point
         self.endPos = point
         self.points.append(point)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "startPos": self.startPos.id,
+            "endPos": self.endPos.id,
+            "points": [point.id for point in self.points]  # Convert points to dictionaries
+        }
+
+    @staticmethod
+    def from_dict(data, lines_data):
+        line_id = data["id"]
+        start_pos = data["startPos"]
+        end_pos = data["endPos"]
+        points = data["points"]  # Deserialize points from dictionaries
+        line = Line(line_id, start_pos, end_pos)
+
+        for i in lines_data[line_id]:
+            line.addPoint(i)
+            i.line = line
+
+        return line
 
 
 class Point:
@@ -35,3 +60,81 @@ class Point:
     def merge(self, point):  # Слияние двух точек (перекрестков)
         self.pos = ((self.pos[0] + point.pos[0]) // 2, (self.pos[1] + point.pos[1]) // 2)
         self.neighbours += point.neighbours
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "pos": self.pos,
+            "line": self.line.id if self.line else None,
+            "isCrossroad": self.isCrossroad,
+            "isAruco": self.isAruco,
+            "arucoAngle": self.arucoAngle,
+            "isEnd": self.isEnd,
+            "neighbours": [neighbour.id for neighbour in self.neighbours]
+        }
+
+    @staticmethod
+    def from_dict(data):
+        point_id = data["id"]
+        pos = data["pos"]
+        line_id = data["line"]
+        is_crossroad = data["isCrossroad"]
+        is_aruco = data["isAruco"]
+        aruco_angle = data["arucoAngle"]
+        is_end = data["isEnd"]
+        neighbour_ids = data["neighbours"]
+
+        point = Point(point_id, pos, None, is_crossroad, is_aruco, aruco_angle, neighbour_ids)
+
+        # if line:
+        #     line.addPoint(point)
+        return point, line_id
+    
+
+def serialize(points, filename="data.json"):
+    points = points.values()
+    res = {"points": [], "lines": []}
+    lines = set()
+    for i in points:
+        res["points"] += [i.to_dict()]
+        lines.add(i.line)
+
+    for i in lines:
+        res["lines"] += [i.to_dict()]
+    print(res)
+    with open(filename, 'w') as json_file:
+        json.dump(res, json_file, indent=4)
+
+
+def deserialize(filename):
+    with open(filename, 'r') as json_file:
+        data = json.load(json_file)
+        points = {}
+        lines_data = {}
+        for i in data["points"]:
+            point, line_id = Point.from_dict(i)
+            lines_data[line_id] = lines_data.get(line_id, []) + [point]
+
+        for i in data["lines"]:
+            Line.from_dict(i, lines_data)
+        return points
+    
+
+if __name__ == "__main__":
+    line1 = Line(1)
+    line2 = Line(2)
+    point1 = Point(1, (0, 0), line1)
+    point2 = Point(2, (1, 1), line1)
+    point3 = Point(3, (2, 2), line2)
+
+    # Добавление точек к линиям
+    line1.addPoint(point1)
+    line1.addPoint(point2)
+    line2.addPoint(point3)
+
+    # Сериализация в JSON
+    serialize({point1.id: point1, point2.id: point2, point3.id: point3}, "data.json")
+
+    # Десериализация из JSON
+    res = deserialize("data.json")
+    print(res)
--- server/main.py
+++ server/main.py
@@ -169,7 +169,7 @@ def init(task):
 def debugLocal():
     DEBUG = settings().DEBUG
 
-    img = cv2.imread("images/86.png")
+    img = cv2.imread("images/87.png")
 
     # route = ...  # загрузить из task
     route = [
@@ -183,6 +183,7 @@ def debugLocal():
     markupArray = getMarkupPositions(img)
     markerCorners, markerIds = findArucoMarkers(img)
     dictAruco = detectAruco(img, markerCorners, markerIds, 100)
+    print(dictAruco)
     # dictAruco = detectAruco(img, 150, show=True)
 
     # robotPos, angle = detectRobot(img)
@@ -216,3 +217,136 @@ if __name__ == '__main__':
         debugLocal()
     else:
         solve()
+
+'''
+import socket
+import os
+import time
+import math
+
+from const import ConstPlenty
+from aruco import findArucoMarkers, detectAruco
+from vision import detectRobot
+from funcs import getDistanceBetweenPoints, getErrorByPoints, angleToPoint
+import numpy as np
+import cv2
+
+from nto.final import Task
+
+const = ConstPlenty()
+task = Task()
+
+class Robot:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.stop()
+
+    def send(self, message):
+        self.sock.sendto(message, (self.ip, self.port))
+
+    def turnRight(self, speed):
+        strPath = ':'.join(['1', str(speed)])
+        self.send(strPath.encode('utf-8'))
+
+    def turnLeft(self, speed):
+        strPath = ':'.join(['0', str(speed)])
+        self.send(strPath.encode('utf-8'))
+
+    def stop(self):
+        for dir in '01':
+            strPath = ':'.join([dir, '0'])
+            self.send(strPath.encode('utf-8'))
+
+
+class Camera:
+    def __init__(self, index):
+        self.cap = cv2.VideoCapture(index)
+        self.cap.set(cv2.CAP_PROP_BRIGHTNESS, 20)
+        self.cap.set(cv2.CAP_PROP_FOCUS, 0)
+        self.cap.set(cv2.CAP_PROP_SATURATION, 0)
+
+    def read(self):
+        success, img = self.cap.read()
+        return img if success else None
+
+    def release(self):
+        self.cap.release()
+
+robot = Robot('10.128.73.116', 5005)
+cam = Camera(index=2)
+
+def saveImage(img):
+    cv2.imwrite(os.path.join(const.path.images, f'Camera.png'), img)
+
+def showImage(img, winName='ImageScene'):
+    cv2.imshow(winName, img)
+    if cv2.waitKey(1) == 27: raise ValueError('Exit by user')
+
+def angleRegulator(error, maxSpeed, kp=1):
+    if abs(error) < math.radians(20): speed = maxSpeed
+    else: speed = 0
+    u = error * kp
+    print(speed + u, speed - u)
+    robot.turnRight(speed + u)
+    robot.turnLeft(speed - u)
+
+def rotate360(speed, timer=16, show=False):
+    lastTime = time.time()
+    robot.turnRight(speed)
+    robot.turnLeft(-speed)
+    while lastTime + timer > time.time():
+        if show:
+            imgScene = cam.read()
+            showImage(imgScene)
+    robot.stop()
+
+def getResultPath(img, route, show=False):
+    markerCorners, markerIds = findArucoMarkers(img, show=show)
+    arucoPositions = detectAruco(img, markerCorners, markerIds)
+    resultPath = []
+    route = [{'marker_id': 2}, {'marker_id': 55}, {'marker_id': 205}]
+    for aruco in route:
+        markerId = aruco['marker_id']
+        if f'p_{markerId}' in arucoPositions:
+            resultPath.append(arucoPositions[f'p_{markerId}'])
+    return resultPath
+
+def driveToArucoMarkers(path, speed, show=False):
+    for arucoMarker in path:
+        imgScene = cam.read()
+        posAruco, angleAruco = arucoMarker
+        centerRobot, directionPoint = detectRobot(imgScene)
+        while getDistanceBetweenPoints(centerRobot, posAruco) < 10:
+            imgScene = cam.read()
+            centerRobot, directionPoint = detectRobot(imgScene)
+            error = getErrorByPoints(directionPoint, posAruco, centerRobot)
+            angleRegulator(error, speed)
+            if show: showImage(imgScene)
+        robot.stop()
+        directionArucoPoint = angleToPoint(posAruco, angleAruco)
+        while True:
+            imgScene = cam.read()
+            centerRobot, directionPoint = detectRobot(imgScene)
+            error = getErrorByPoints(directionPoint, directionArucoPoint, centerRobot)
+            angleRegulator(error, 0)
+            if show: showImage(imgScene)
+            if abs(error) < math.radians(10): break
+        robot.stop()
+        rotate360(speed, show=show)
+
+def solve():
+    task.start()
+    route = task.getTask()
+    imgScene = cam.read()
+    saveImage(imgScene)
+    resultPath = getResultPath(imgScene, route, show=True)
+    print(resultPath)
+    driveToArucoMarkers(resultPath, speed=120, show=True)
+    task.stop()
+
+if __name__ == '__main__':
+    solve()
+
+'''
